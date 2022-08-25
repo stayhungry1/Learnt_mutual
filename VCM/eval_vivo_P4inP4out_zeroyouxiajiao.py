9cd0602ccc82f51b697c7ec5c139a3d274c5d687
SeasonIrving <wangletian@buaa.edu.cn>
2022-08-25 19:43:28 +0800

    lt

--- VCM/eval_vivo_P4inP4out_zeroyouxiajiao.py
+++ VCM/eval_vivo_P4inP4out_zeroyouxiajiao.py
@@ -297,10 +297,10 @@ class Eval:
         guiyihua_scale = guiyihua_max - guiyihua_min
         ###pad
         d_originalsize = d
-        d, h_new_left, h_new_right, w_new_left, w_new_right = Pfeature_zeropad_youxiajiao128(d, 16)
-        d_p4, _, _, _, _ = Pfeature_zeropad_youxiajiao128(d_p4, 16)
-        # d, h_new_left, h_new_right, w_new_left, w_new_right = Pfeature_zeropad_youxiajiao(d, 16)
-        # d_p4, _, _, _, _ = Pfeature_zeropad_youxiajiao(d_p4, 16)
+        # d, h_new_left, h_new_right, w_new_left, w_new_right = Pfeature_zeropad_youxiajiao128(d, 16)
+        # d_p4, _, _, _, _ = Pfeature_zeropad_youxiajiao128(d_p4, 16)
+        d, h_new_left, h_new_right, w_new_left, w_new_right = Pfeature_zeropad_youxiajiao(d, 16)
+        d_p4, _, _, _, _ = Pfeature_zeropad_youxiajiao(d_p4, 16)
         d = (d - guiyihua_min) / guiyihua_scale
         d_p4 = (d_p4 - guiyihua_min) / guiyihua_scale
         d_originalsize = (d_originalsize - guiyihua_min) / guiyihua_scale
@@ -330,7 +330,8 @@ class Eval:
         # d_output = torch.zeros(temp_ori_size_p4)  # 用于从网络输出的tensor取出左上角
         net_belle_output = self.model.net_belle(d)
         print(net_belle_output["x_hat"].size(), '-------------------Cheng output (P4) size')
-        d_output = Pfeature_zeropad_youxiajiao128_reverse(net_belle_output["x_hat"], h_new_left, h_new_right, w_new_left, w_new_right)
+        # d_output = Pfeature_zeropad_youxiajiao128_reverse(net_belle_output["x_hat"], h_new_left, h_new_right, w_new_left, w_new_right)
+        d_output = Pfeature_zeropad_youxiajiao_reverse(net_belle_output["x_hat"], h_new_left, h_new_right, w_new_left, w_new_right)
         print('max/min_P4(GT)(Cheng input): %8.4f/%8.4f, max/min_P4(Cheng output): %8.4f/%8.4f'
               % (torch.max(d), torch.min(d), torch.max(d_output), torch.min(d_output)))
         # d_output = net_belle_output["x_hat"][:, :, 0:temp_ori_size_p4[2], 0:temp_ori_size_p4[3]]
