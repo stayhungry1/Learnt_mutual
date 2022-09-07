1ab896751dd2644ab8a5deb11cebdf47bcf679da
SeasonIrving <wangletian@buaa.edu.cn>
2022-09-07 23:38:44 +0800

    lt

--- VCM/test_vivo.py
+++ VCM/test_vivo.py
@@ -37,8 +37,8 @@ if __name__ == "__main__":
         methods_eval = DetectEval(settings, set_idx)
         picklist = sorted(glob.glob(os.path.join(os.environ["DETECTRON2_DATASETS"], "*.jpg")))[:number]
         picklist = [utils.simple_filename(x) for x in picklist]
-        # methods_eval.prepare_part(picklist, data_name="pick") #QP10 QP11 QP13 QP20 QP21 QP12 QP22 QP31 QP32 QP33
-        methods_eval.prepare_part(picklist, data_name="pick1") #QP15 QP16 QP17 QP18 QP101 QP102
+        methods_eval.prepare_part(picklist, data_name="pick") #QP10 QP11 QP13 QP20 QP21 QP12 QP22 QP31 QP32 QP33
+        # methods_eval.prepare_part(picklist, data_name="pick1") #QP15 QP16 QP17 QP18 QP101 QP102
 
     if mode == "feature_coding":
         filenames = methods_eval.feature_coding()
