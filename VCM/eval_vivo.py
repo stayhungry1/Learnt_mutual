7caecbf1af5ea92b6b55e3e0fad3c20155298a89
ltlt <liutie@buaa.edu.cn>
2022-10-16 18:08:12 +0800

    lt

--- VCM/eval_vivo.py
+++ VCM/eval_vivo.py
@@ -274,8 +274,9 @@ class Eval:
         # self.path_bppsave = '../../liutie_save/output/cheng_P2inP3outzeropad128_bpp_lambda1chu2_finenet_iter37999.json'
         # self.path_bppsave = '../../liutie_save/output/cheng_P2inP3outzeropad128_bpp_lambda1chu2_ftlambda1_finenet_iter3999.json'
         # self.path_bppsave = '../../liutie_save/output/cheng_P2inP3outzeropad128_bpp_lambda1chu8_finenet_iter37999.json'
+        self.path_bppsave = '../../liutie_save/output/cheng_P2inP3outzeropad128_bpp_lambda1chu16_finenet_iter37999.json'
         # self.path_bppsave = '../../liutie_save/output/cheng_P2inP3outzeropad128_bpp_lambda4_finenet_iter37999.json'
-        self.path_bppsave = '../../liutie_save/output/cheng_P2inP3outzeropad128_bpp_lambda8_finenet_iter37999.json'
+        # self.path_bppsave = '../../liutie_save/output/cheng_P2inP3outzeropad128_bpp_lambda8_finenet_iter37999.json'
         self.bpp_test5000 = {}
 
     def prepare_dir(self):
